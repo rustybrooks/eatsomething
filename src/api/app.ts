578ce25b3ec17f6da4bee5f747f03ab3914e45bb
Rusty Brooks <me@rustybrooks.com>
2021-10-12 10:46:52 -0500

    stuff mostly runs

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,11 +1,9 @@
 import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
-app.listen(port, () => {
-  console.log(`Timezones by location application is running on port ${port}.`);
-});
+app.listen(port, '0.0.0.0')
 
 interface LocationWithTimezone {
   location: string;
