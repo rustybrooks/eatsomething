06f819507e97cadfd36ac56c8280dc38d1ad5c8e
Rusty Brooks <me@rustybrooks.com>
2022-09-15 18:49:49 -0500

    refactor user/route into user/mod

--- src/api/src/user/mod.rs
+++ src/api/src/user/mod.rs
@@ -1,17 +1,58 @@
-use crate::pool::OurPool;
 use core::clone::Clone;
+
 use warp::{self, Filter};
+
+use crate::pool::OurPool;
+use crate::routes;
+use crate::routes::{with_auth, with_db, with_json_body};
+use crate::user::handlers::{AddFriendReq, UserLoginReq, UserSignupReq};
 
 mod handlers;
 pub mod models;
-mod routes;
 
 pub fn routes(pool: OurPool) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path("user").and(
-        routes::signup(pool.clone())
-            .or(routes::login(pool.clone()))
-            .or(routes::me(pool.clone()))
-            .or(routes::add_friend(pool.clone()))
-            .or(routes::friends(pool.clone())),
+        signup(pool.clone())
+            .or(login(pool.clone()))
+            .or(me(pool.clone()))
+            .or(add_friend(pool.clone()))
+            .or(friends(pool.clone())),
     )
+}
+
+pub fn signup(pool: OurPool) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path("signup")
+        .and(warp::post())
+        .and(routes::with_db(pool))
+        .and(routes::with_json_body::<UserSignupReq>())
+        .and_then(handlers::signup)
+}
+
+pub fn login(pool: OurPool) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path("login")
+        .and(warp::post())
+        .and(with_db(pool))
+        .and(with_json_body::<UserLoginReq>())
+        .and_then(handlers::login)
+}
+
+pub fn me(pool: OurPool) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path("me").and(warp::get()).and(with_db(pool)).and(with_auth()).and_then(handlers::me)
+}
+
+pub fn add_friend(pool: OurPool) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path("add_friend")
+        .and(warp::get())
+        .and(with_db(pool))
+        .and(with_auth())
+        .and(warp::query::<AddFriendReq>())
+        .and_then(handlers::add_friend)
+}
+
+pub fn friends(pool: OurPool) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path("friends")
+        .and(warp::get())
+        .and(with_db(pool))
+        .and(with_auth())
+        .and_then(handlers::friends)
 }
